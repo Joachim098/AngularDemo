7664a4e3c59061376b7d59b903755d3188d38133
Joachim Ngabo <joachimngbao@gmail.com>
2021-10-27 17:07:01 +0200

    added salarylist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { SalaryModule } from './salary/salary.module';
+import { RouterModule } from '@angular/router';
+import { SalaryListComponent } from './salary/salary-list/salary-list.component';
+import { TaxComponent } from './salary/tax.component';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +16,12 @@ import { SalaryModule } from './salary/salary.module';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    SalaryModule
+    SalaryModule,
+    RouterModule.forRoot([
+      {path: 'salary-record', component: TaxComponent},
+      {path: 'salary-list', component: SalaryListComponent},
+      {path: '', redirectTo: 'salary-record', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
