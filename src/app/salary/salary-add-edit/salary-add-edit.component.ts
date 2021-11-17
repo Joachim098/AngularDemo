d7fb06529bcaa979f88b4fefd5eae4560cfc6574
Joachim Ngabo <joachimngbao@gmail.com>
2021-11-17 17:55:46 +0200

    Fixed

--- src/app/salary/salary-add-edit/salary-add-edit.component.ts
+++ src/app/salary/salary-add-edit/salary-add-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute} from '@angular/router';
 import { FormData } from '../form-data';
 import { Salary } from '../salary';
